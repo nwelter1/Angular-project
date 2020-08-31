d6402ca2c3292bfe0f55ce808db898c02687a5cf
nwelter1 <nwelter1@gmail.com>
2020-08-31 09:59:09 -0500

    made some small changes, CRUD not allowing home page to display - Google Sign in app not working either

---
+++ environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "HIDING API KEY",
+      authDomain: "HIDING DOMAIN",
+      databaseURL: "HIDING DATABASE",
+      projectId: "HIDING PROJECT ID",
+      storageBucket: "HIDING STORAGE BUCKET",
+      messagingSenderId: "HIDING SENDER ID",
+      appId: "HIDING APP ID"
+    }
+  };

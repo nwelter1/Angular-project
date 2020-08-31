250dd6e752e7b88e9e0e1cf1a38511857662f511
nwelter1 <nwelter1@gmail.com>
2020-08-31 17:35:19 -0500

    SHould be updated with proper API key... issues with display still persist - says errors with API key

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-      apiKey: "HIDING API KEY",
-      authDomain: "HIDING DOMAIN",
-      databaseURL: "HIDING DATABASE",
-      projectId: "HIDING PROJECT ID",
-      storageBucket: "HIDING STORAGE BUCKET",
-      messagingSenderId: "HIDING SENDER ID",
-      appId: "HIDING APP ID"
+      apiKey: "AIzaSyAmU5imAt8Jy2G2fb4Hp8j4Mx8YiBhK4Bo",
+      authDomain: "employee-db-nw.firebaseapp.com",
+      databaseURL: "https://employee-db-nw.firebaseio.com",
+      projectId: "employee-db-nw",
+      storageBucket: "employee-db-nw.appspot.com",
+      messagingSenderId: "260804080577",
+      appId: "1:260804080577:web:d30abc48b1c797e6247540"
     }
   };
